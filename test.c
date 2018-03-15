1fa889b1ad7e0c78b0bd1caf5f24769ed40ff081
amu340 <amulee340@gmail.com>
2018-03-15 02:29:34 -0700

    first

---
+++ test.c
@@ -0,0 +1,13 @@
+/*************************************************************************
+	> File Name: test.c
+	> Author: 
+	> Mail: 
+	> Created Time: Thu 15 Mar 2018 02:27:07 AM PDT
+ ************************************************************************/
+
+#include<stdio.h>
+int main(int argc, char** argv) {
+    printf("hello world!\n");
+
+    return 0;
+}
